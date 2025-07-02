8f2203f781a7ac450e6b0eab13849f609a6d4016
Siddharth Jain <155904393+sidjain1239@users.noreply.github.com>
2025-07-02 20:46:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 app = FastAPI()
 
-# Load label encoder
-df = pd.read_csv("ai_service_all_classes_540.csv")
-le = LabelEncoder()
-le.fit(df["service"])
+classifier = pipeline("text-classification", model="./distilbert_ai_router", tokenizer="./distilbert_ai_router")
 
-# Load pipeline
-classifier = pipeline(
-    "text-classification",
-    model="./distilbert_ai_router",
-    tokenizer="./distilbert_ai_router",
-    framework="pt",
-    device=-1  # CPU
-)
-
-class Prompt(BaseModel):
-    text: str
+class PromptInput(BaseModel):
+    prompt: str
 
 @app.get("/")
-def home():
-    return {"message": "AI Prompt Classifier API is up."}
+def read_root():
+    return {"message": "FastAPI app is running"}
 
 @app.post("/predict")
-def predict_service(prompt: Prompt):
-    result = classifier(prompt.text)[0]
-    predicted_id = int(result["label"].split("_")[-1])
-    predicted_label = le.inverse_transform([predicted_id])[0]
-    return {"service": predicted_label}
+def predict(data: PromptInput):
+    return classifier(data.prompt)
